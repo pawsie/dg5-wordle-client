2708bbd9b39fd16805881e20be0353b8ae6b7669
pawsie <pawsielee@gmail.com>
2022-08-12 15:54:59 +0800

    add hard coded letter and word components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
+import { LetterComponent } from './letter/letter.component';
+import { WordComponent } from './word/word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LetterComponent,
+    WordComponent
   ],
   imports: [
     BrowserModule,
